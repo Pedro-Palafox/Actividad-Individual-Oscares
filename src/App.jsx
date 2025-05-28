4ef255969997b5b0ee5e08f676126c089ae6357a
Pedro Palafox <A01027841@tec.mx>
2025-05-27 22:53:39 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ScreenLayout } from './layouts/ScreenLayout'
+
+export const App = () => {
+  return (
+    <div>
+      <ScreenLayout>
+        
+      </ScreenLayout>
+    </div>
+  )
+}
+
+
---
+++ src/layouts/ScreenLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+
+export const ScreenLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+
